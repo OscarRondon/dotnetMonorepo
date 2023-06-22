bb66064a6a68d17a7efb1e30a5e32040c2cfe796
orondon <oscar.rondon.c@gmail.com>
2023-06-22 11:03:27 -0400

    Session 1-10

---
+++ csharp101/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+var aFriend = "<My friend Name>";
+Console.WriteLine($"Hello, my dear {aFriend}!");
+Console.WriteLine($"Hello, my dear {aFriend.ToUpper()}!");
+Console.WriteLine($"Hello, my dear {aFriend.ToLower()}!");
