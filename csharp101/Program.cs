824b62de71578c07911819c78f98b76372ffcf4c
orondon <oscar.rondon.c@gmail.com>
2023-06-23 19:29:24 -0400

    Session 11-12

--- csharp101/Program.cs
+++ csharp101/Program.cs
@@ -3,3 +3,9 @@ var aFriend = "<My friend Name>";
 Console.WriteLine($"Hello, my dear {aFriend}!");
 Console.WriteLine($"Hello, my dear {aFriend.ToUpper()}!");
 Console.WriteLine($"Hello, my dear {aFriend.ToLower()}!");
+
+var names = new List<string> { "<name 1>", "<name 2>" , "<name 3>" };
+foreach(string name in names )
+{
+    Console.WriteLine(name);
+}
