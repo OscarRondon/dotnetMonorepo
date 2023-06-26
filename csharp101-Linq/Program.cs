834ae72d5321b9f42ed3598013e62ca794f42550
orondon <oscar.rondon.c@gmail.com>
2023-06-26 17:16:46 -0400

    cshap 101 part 2 session 1

---
+++ csharp101-Linq/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string[] digits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        var shortDigits = digits.Where((digit, index) => digit.Length < index);
+
+        Console.WriteLine("Short digits:");
+        foreach (var d in shortDigits)
+        {
+            Console.WriteLine($"The word {d} is shorter than its value.");
+        }
+
+        int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+        string[] strings = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        var textNums = from n in numbers
+                       select strings[n];
+
+        Console.WriteLine("Number strings:");
+        foreach (var s in textNums)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
