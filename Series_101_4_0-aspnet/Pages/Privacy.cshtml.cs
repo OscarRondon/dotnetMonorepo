93675b966500fca8f6b3b69e0cadfad85851d56c
orondon <oscar.rondon.c@gmail.com>
2023-06-29 09:13:26 -0400

    ASP.Net [101 Series project creation]

---
+++ Series_101_4_0-aspnet/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Series_101_4_0_aspnet.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
