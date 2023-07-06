46ee41049fef7c1c07fd4bb55f5d781ca81f2440
orondon <oscar.rondon.c@gmail.com>
2023-07-05 21:05:57 -0400

    Web API [ Videos 1-4]

---
+++ Series_101_5_0-webapi/Controllers/RecipesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Series_101_5_0_webapi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+
+        [HttpGet("Dishes")]
+        public string[] Get()
+        {
+            return new string[] { "Pollo Frito", "Sushi", "Pizza" };
+        }
+
+        [HttpGet("Dishes2")]
+        public string[] Get(string id)
+        {
+            return new string[] { "Pollo Frito", "Sushi", "Pizza" };
+        }
+    }
+}
