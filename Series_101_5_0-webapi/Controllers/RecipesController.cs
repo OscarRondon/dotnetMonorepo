091fa7808a0b9c00a4e3bcaa403a84d12c75fd32
orondon <oscar.rondon.c@gmail.com>
2023-07-07 08:50:13 -0400

    Web API [Videos 15 - 18]

--- Series_101_5_0-webapi/Controllers/RecipesController.cs
+++ Series_101_5_0-webapi/Controllers/RecipesController.cs
@@ -2,10 +2,13 @@
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Series_101_5_0_webapi.Models;
+using System.Net.Mime;
 
 namespace Series_101_5_0_webapi.Controllers
 {
     [Route("[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
     [ApiController]
     public class RecipesController : ControllerBase
     {
@@ -15,7 +18,19 @@ namespace Series_101_5_0_webapi.Controllers
             new Recipe() {Title = "Pizza" }
         };
 
+        /// <summary>
+        /// Get all recipes
+        /// </summary>
+        /// <returns>List of recipes</returns>
+        /// <remarks>
+        /// Example:
+        /// 
+        /// GET https://localhost:port/Recipes
+        /// 
+        /// </remarks>
         [HttpGet("/[controller]",Name = "GetRecipesAll", Order = 0)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult Get()
         {
             if (!recipes.Any())
@@ -41,6 +56,21 @@ namespace Series_101_5_0_webapi.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Update a recipe
+        /// </summary>
+        /// <returns>List of recipes</returns>
+        /// <remarks>
+        /// Example:
+        ///
+        ///     PATCH https://localhost:port/Recipes(125)
+        ///     {
+        ///        "path": "/title",
+        ///        "op": "replace",
+        ///        "value": "The new title!!!"
+        ///     }
+        ///     
+        /// </remarks>
         [HttpPatch("/[controller]({id:int})", Name = "UpdateRecipe", Order = 3)]
         public ActionResult Update(int id, JsonPatchDocument<Recipe> recipesUpdate)
         {
--- Series_101_5_0-webapi/Program.cs
+++ Series_101_5_0-webapi/Program.cs
@@ -1,5 +1,7 @@
 
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
 
 namespace Series_101_5_0_webapi
 {
@@ -17,7 +19,34 @@ namespace Series_101_5_0_webapi
             builder.Services.AddControllers().AddNewtonsoftJson();
             builder.Services.AddSwaggerGen(opt =>
             {
-                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "myWebApi", Version = "v1" });
+                opt.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Title = "myWebApi", 
+                    Description = "A Sample ASP.Net Web Api",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Oscar Rondon",
+                        Email = "oscar.rondon.c@treeoflifesoftware.dev",
+                        Url = new Uri("https://github.com/OscarRondon")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT Licence",
+                        Url = new Uri("https://opensource.org/licenses/MIT")
+                    },
+                    Version = "v1" 
+                });
+
+                //Generate the xml docs tha'll drive the swagger docs
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile) ;
+                opt.IncludeXmlComments(xmlPath);
+
+                opt.CustomOperationIds(apiDesc =>
+                {
+                    return apiDesc.TryGetMethodInfo(out MethodInfo methodInfo) ? methodInfo.Name : null;
+                });
+
             }).AddSwaggerGenNewtonsoftSupport();
             
 
@@ -30,7 +59,7 @@ namespace Series_101_5_0_webapi
                 app.UseSwaggerUI(opt =>
                 {
                     opt.SwaggerEndpoint("/swagger/v1/swagger.json", "myWebApi v1" );
-                    
+                    opt.DisplayOperationId();
                 });
             }
             else
