ec33fa60cf35110492dd5301dadf738b59e6bdba
orondon <oscar.rondon.c@gmail.com>
2023-07-10 10:20:03 -0400

    Blazor [Videos 5 - 6]

---
+++ Series_101_6_0-Blazor/Model/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Series_101_6_0_Blazor.Model
+{
+    public class TodoItem
+    {
+        public string Id { get; set; } = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+        public string Todo { get; set; }
+        public DateTime ?DueDate { get; set; }
+    }
+}
