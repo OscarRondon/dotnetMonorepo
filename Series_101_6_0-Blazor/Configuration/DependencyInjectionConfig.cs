ed1d3c921f0950c8118909a527dc1727792a152b
orondon <oscar.rondon.c@gmail.com>
2023-07-27 17:38:26 -0400

    Blazor [Dependency Injection]

---
+++ Series_101_6_0-Blazor/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,15 @@
+﻿using Series_101_6_0_Blazor.Data;
+
+namespace Series_101_6_0_Blazor.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+
+        public static IServiceCollection AddWheatherForecastServices(this IServiceCollection services)
+        {
+            services.AddTransient<WeatherForecastService>();
+            return services;
+        }
+
+    }
+}
